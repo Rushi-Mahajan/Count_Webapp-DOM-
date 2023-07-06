6f4763e726831017e3da8163280c18ef11f0ed2c
Hrushikesh Mahajan <43132409+Rushi-Mahajan@users.noreply.github.com>
2023-07-06 15:59:01 +0530

    Add files via upload

---
+++ Statement 2/Doubleclick/scripts.js
@@ -0,0 +1,21 @@
+let Inc_bt1=document.getElementById("Button-1")
+let Dec_bt2=document.getElementById("ButtonAdd1")
+let Res_bt3=document.getElementById("resetButton")
+
+
+function increment(){
+    document.querySelector(".Text").innerText=Number(document.querySelector(".Text").innerText)+1
+}
+
+function decrement(){
+    document.querySelector(".Text").innerText=Number(document.querySelector(".Text").innerText)-1
+}
+
+function reset(){
+    document.querySelector(".Text").innerText=0
+}
+
+Inc_bt1.addEventListener("dblclick",decrement)
+Dec_bt2.addEventListener("dblclick",increment)
+Res_bt3.addEventListener("dblclick",reset)
+
